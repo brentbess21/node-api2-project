1275d279b04a8e806404b6297064c46d017d542e
Brent Besselievre <brent@brents-mbp.lan>
2021-09-28 12:50:10 -0600

    Added GET /api/posts endpoint to server.js

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,8 @@
 // implement your posts router here
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/api/posts', async (req, res)=> {
+    
+})
--- api/server.js
+++ api/server.js
@@ -2,10 +2,22 @@
 // require your posts router and connect it here
 
 const express = require('express')
+const Post = require('./posts/posts-model')
 
 const server = express();
 
 server.use(express.json())
 
+server.get('/api/posts', async (req, res)=> {
+    try {
+        const posts = await Post.find();
+        res.status(200).json(posts)
+    }
+    catch{
+        res.status(500).json({
+            message: "The posts information could not be retrieved"
+        })
+    }
+})
 
 module.exports = server;
