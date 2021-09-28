7cc8aa2f11e6dd4a962394525d41636eac22784d
Brent Besselievre <brent@brents-mbp.lan>
2021-09-28 15:57:53 -0600

    Passing all tests except 8, 13, and 16

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -75,5 +75,74 @@ router.post('/', (req, res)=> {
                 })
             })
     }
+});
+
+router.put('/:id', async (req, res)=> {
+    try{
+        const { title, contents } = req.body
+        const { id } = req.params
+
+        if (!title || !contents) {
+            res.status(400).json({
+                message: "Please provide title and contents for the post"
+            })
+        } else {
+            const updatedPost = await Post.update(id, {title, contents});
+            if (!updatedPost) {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist"
+                })
+            } else { 
+                res.status(200).json(updatedPost)
+            }
+        }
+        
+
+    }
+    catch (err) {
+        res.status(500).json({
+            message: "The post information could not be modified"
+        })
+    }
+});
+
+router.delete("/:id", async (req, res)=> {
+    try{
+        const { id } = req.params;
+        const deletedPost = await Post.remove(id)
+
+        if(!deletedPost) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        } else {
+            res.status(200).json(deletedPost);
+        }
+
+    }
+    catch (err) {
+        res.status(500).json({
+            message: "The post could not be removed"
+        })
+    }
+});
+
+router.get('/:id/comments', async (req, res)=> {
+    try {
+        const { id } = req.params
+        const post = await Post.findById(id);
+        if (!post) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        } else {
+        
+        }
+    }
+    catch{
+        res.status(500).json({
+            message: "The comments information could not be retrieved"
+        })
+    }
 })
 module.exports = router;
