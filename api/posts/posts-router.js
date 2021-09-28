342a75a69cf2f95db0b84115aaaa423dc8734a38
Brent Besselievre <brent@brents-mbp.lan>
2021-09-28 13:17:28 -0600

    Added GET /api/posts/:id endpoint with router. Tests 1-4 are passing

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -15,4 +15,23 @@ router.get('/', async (req, res)=> {
     }
 })
 
+router.get('/:id', async (req, res)=> {
+    try {
+        const { id } = req.params;
+        const post = await Post.findById(id);
+        if (!post) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        } else {
+            res.status(200).json(post)
+        }
+    }
+    catch {
+        res.status(500).json({
+            message: "The post information could not be retrieved"
+        })
+    }
+})
+
 module.exports = router;
